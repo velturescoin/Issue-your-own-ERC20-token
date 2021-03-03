c6fe3802dc849dca10e5447b754de0b4f3b1613a
velturescoin <79715465+velturescoin@users.noreply.github.com>
2021-03-04 04:39:00 +0700

    Update erc20_tutorial.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -113,10 +113,10 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+        symbol = "Veltures Coin";
+        name = "VERCO";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
+        _totalSupply = 100000000;
         balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
         emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
     }
